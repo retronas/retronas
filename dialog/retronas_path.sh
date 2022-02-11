29c04f023907cbe6d608121aefb0e9fffbef32b2
sairuk <sairuk@gmail.com>
2022-02-11 23:49:35 +1100

    migrate path ui

--- dialog/retronas_path.sh
+++ dialog/retronas_path.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-clear
-source /opt/retronas/dialog/retronas.cfg
+_CONFIG=/opt/retronas/dialog/retronas.cfg
+source $_CONFIG
+source ${DIDIR}/common.sh
 cd ${DIDIR}
 
 rn_retronas_path() {
-dialog \
-  --backtitle "RetroNAS" \
-  --clear \
-  --title "Please type in the RetroNAS top level directory" \
-  --dselect "${OLDRNPATH}" ${MG} 2>${TDIR}/rn_retronas_path
-  # Strip the trailing slash
-  cat ${TDIR}/rn_retronas_path | sed 's#/$##g' > ${TDIR}/rn_retronas_path2
-  mv ${TDIR}/rn_retronas_path2 ${TDIR}/rn_retronas_path
+  DLG_DSELECT "Please type in the RetroNAS top level directory" "${OLDRNPATH}"
 }
-
-rn_retronas_path_confirm() {
-NEWRNPATH=$( cat ${TDIR}/rn_retronas_path )
-dialog \
-  --clear \
-  --title "Confirm" \
-  --defaultno \
-  --yesno "Do you want to save this setting? \
-  \nNewRetroNAS top level directory: \"${NEWRNPATH}\" " ${MG}
-}
-
-echo "${OLDRNPATH}" > ${TDIR}/rn_retronas_path
 
 # Show the ugly path chooser
 rn_retronas_path
+case $CHOICE in
+  0)
+    NEWRNPATH=$CHOICE
+    unset ${CHOICE}
+    # Confirm the input because the path chooser sucks
+    rn_retronas_path_confirm "${NEWRNPATH}"
+    ;;
+  *)
+    exit ${CHOICE}
+    ;;
+esac
 
-# Confirm the input because the path chooser sucks
-rn_retronas_path_confirm
-CHOICE="$?"
+rn_retronas_path_confirm() {
+  local NEWRNPATH="${1}"
+
+  local MENU_BLURB="\nDo you want to save this setting? \
+  \nNewRetroNAS top level directory: \"${NEWRNPATH}\""
+
+  DLG_YN "Confirm" "${MENU_BLURB}"
+}
 
 case ${CHOICE} in
   0)
@@ -41,7 +39,7 @@ case ${CHOICE} in
     sed -i '/retronas_path:/d' "${ANCFG}"
     # Add the new value and re-source
     echo "retronas_path: \"${NEWRNPATH}\"" >> "${ANCFG}"
-    source /opt/retronas/dialog/retronas.cfg
+    source $_CONFIG
     exit 0
     ;;
   *)
