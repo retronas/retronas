54b6d64399007e30bd825a5d66d87902a7699b42
sairuk <sairuk@gmail.com>
2022-02-11 23:59:38 +1100

    migrate tools ui

--- dialog/tools.sh
+++ dialog/tools.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 
 clear
-source /opt/retronas/dialog/retronas.cfg
+_CONFIG=/opt/retronas/dialog/retronas.cfg
+source $_CONFIG
+source ${DIDIR}/common.sh
 cd ${DIDIR}
 
 rn_tools() {
-source /opt/retronas/dialog/retronas.cfg
-dialog \
-  --backtitle "RetroNAS" \
-  --title "RetroNAS Tools menu" \
-  --clear \
-  --menu "My IP addresses: ${MY_IPS} \
-  \n
-  \nPlease select an service to check" ${MG} 10 \
-  "01" "Main Menu" \
-  "02" "GOG - Download your GOG installers and extras" \
-  "03" "Nintendo 3DS QR code generator for FBI homebrew" \
-  "04" "Configure a TCPServ Software Modem" \
-  "05" "ROM import tool via Smokemonster SMDBs" \
-  2> ${TDIR}/rn_tools
+  source $_CONFIG
+
+  local MENU_ARRAY=(
+    01 "Main Menu"
+    02 "GOG - Download your GOG installers and extras"
+    03 "Nintendo 3DS QR code generator for FBI homebrew"
+    05 "ROM import tool via Smokemonster SMDBs"
+  )
+
+  local MENU_BLURB="Please select an option to install"
+
+  DLG_MENU "Tools Menu" $MENU_ARRAY 10 "${MENU_BLURB}"
+
 }
 
-## If this is run as root, switch to our RetroNAS user
-## Manifests and cookies stored in ~/.gogrepo
-if [ "${USER}" == "root" ]
-then
-  SUCOMMAND="sudo -u ${OLDRNUSER}"
-else
-  SUCOMMAND=""
-fi
-
-SC="systemctl --no-pager --full"
-
+DROP_ROOT
 while true
 do
   rn_tools
-  CHOICE=$( cat ${TDIR}/rn_tools )
-  PAUSEMSG='Press [Enter] to continue...'
   case ${CHOICE} in
   02)
     # gogrepo
@@ -46,16 +35,11 @@ do
     # 3DS QR
     clear
     ${SUCOMMAND} ../scripts/3ds_qr.sh
-    echo "${PAUSEMSG}"
-    read -s
-    ;;
-  04)
-    # TCPSer Setup
-    bash tcpser.sh
+    PAUSE
     ;;
   05)
     # ROM import SMDB
-    clear
+    CLEAR
     bash romimport.sh
     ;;
   *)
