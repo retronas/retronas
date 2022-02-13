846d4910df127eddda7257378bfb54c5c39ee533
sairuk <sairuk@gmail.com>
2022-02-13 17:47:27 +1100

    migrate _only_ fixperms dialog

--- dialog/retronas_fixperms.sh
+++ dialog/retronas_fixperms.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-clear
+set -u
 
-source /opt/retronas/dialog/retronas.cfg
+_CONFIG=/opt/retronas/dialog/retronas.cfg
+source $_CONFIG
+source ${DIDIR}/common.sh
 cd ${DIDIR}
 
 rn_get_dirs() {
-cd "${OLDRNPATH}"
-find . -maxdepth 1 -type d | sed 's#^\./##g' | grep -v ^\.$ | sort | while read PATHITEM
-do
-  echo "${PATHITEM}" "${PATHITEM}" >> ${TDIR}/rn_get_dirs
-  COUNT=$((${COUNT}+1))
-done
-
-cat ${TDIR}/rn_get_dirs
+  COUNT=2
+  cd "${OLDRNPATH}"
+  find . -maxdepth 1 -type d | sed 's#^\./##g' | grep -v ^\.$ | sort | while read PATHITEM
+  do
+    echo "${PATHITEM}" "${PATHITEM}"
+    COUNT=$((${COUNT}+1))
+  done
 }
 
 rn_fix_perms() {
-cd ${DIDIR}
-dialog \
-  --backtitle "RetroNAS" \
-  --title "RetroNAS Fix Permissions" \
-  --clear \
-  --menu "Please choose a directory below ${OLDRNPATH} to fix. \
-  \n
-  \nThis will reset all the file ownership to the user \"${OLDRNUSER}\" \
-  \n
-  \nPlease be careful, as this is irreversible.  If unsure, exit now." \
-  ${MG} 5 \
-  "1" "Exit to main menu" \
-  $( cat  ${TDIR}/rn_get_dirs ) 2>${TDIR}/rn_fix_perms
+  cd ${DIDIR}
+
+  local MENU_ARRAY=(
+    1 "Exit to main menu"
+    $(rn_get_dirs)
+  )
+
+  local MENU_BLURB="\nPlease choose a directory below ${OLDRNPATH} to fix. \
+  \n\nThis will reset all the file ownership to the user \"${OLDRNUSER}\" \
+  \n\nPlease be careful, as this is irreversible.  If unsure, exit now."
+
+  DLG_MENU "Fix Permissions" $MENU_ARRAY 5 "${MENU_BLURB}" 
 }
 
-rn_get_dirs
-
+CLEAR
 rn_fix_perms
-CHOICE=$( cat ${TDIR}/rn_fix_perms )
 case ${CHOICE} in
   1)
     exit 1
     ;;
   *)
-    clear
+    CLEAR
     chown -Rc ${OLDRNUSER}:${OLDRNUSER} "${OLDRNPATH}/${CHOICE}"
     chmod -Rc a-st,u+rwX,g+rwX,o+rX "${OLDRNPATH}/${CHOICE}"
     ;;
