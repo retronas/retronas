9d08148407f7095d5031ed99635a4b7fa0e95f63
sairuk <sairuk@gmail.com>
2022-02-13 17:44:43 +1100

    Revert "migrate fixperms dialog"
    This reverts commit a487ebd41e43a00195acf142dae15f20af36d54a.

--- dialog/gogrepo.sh
+++ dialog/gogrepo.sh
@@ -1,161 +1,165 @@
 #!/bin/bash
 
-
-_CONFIG=/opt/retronas/dialog/retronas.cfg
-source $_CONFIG
-source ${DIDIR}/common.sh
+clear
+source /opt/retronas/dialog/retronas.cfg
 cd ${DIDIR}
 
-DROP_ROOT
+## If this is run as root, switch to our RetroNAS user
+## Manifests and cookies stored in ~/.gogrepo
+if [ "${USER}" == "root" ]
+then
+  SUCOMMAND="sudo -u ${OLDRNUSER}"
+else
+  SUCOMMAND=""
+fi
+
 
 rn_gog_chooser() {
-  source $_CONFIG
-
-  local MENU_ARRAY=(
-    01 "Exit to previous menu"
-    02 "Configure my GOG credentials"
-    03 "Change my GOG Operating System setting"
-    04 "Synchronise my games list"
-    05 "Download/update a single game"
-    06 "Download/update all games currently in my games list"
-    07 "FULL - Synchronise games list & download/update all games"
-  )
-
-  local MENU_BLURB="Current Operating System: ${OLDGOGOS} \
+source /opt/retronas/dialog/retronas.cfg
+dialog \
+  --backtitle "RetroNAS" \
+  --title "gogrepo chooser" \
+  --clear \
+  --menu "Current OS: ${OLDGOGOS} \
   \n
-  \nPlease choose a task"
-
-  DLG_MENU "gogrepo configure OS" $MENU_ARRAY 10 "${MENU_BLURB}"
-
-  while true
-  do
-    source $_CONFIG
-    case ${CHOICE} in
-    02)
-      # login
-      CLEAR
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_login.sh
-      PAUSE
-      ;;
-    03)
-      # OS
-      rn_gog_setos
-      ;;
-    04)
-      # sync games list
-      CLEAR
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -skipknown -os ${OLDGOGOS}
-      PAUSE
-      ;;
-    05)
-      # download 1 game
-      rn_gog_gameslist
-      rn_gog_game
-      GOGGAME=$( cat ${TDIR}/rn_gog_game )
-      CLEAR
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -os ${OLDGOGOS} -id ${GOGGAME}
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh -id ${GOGGAME}
-      PAUSE
-      ;;
-    06)
-      # download all games
-      CLEAR
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh
-      PAUSE
-      ;;
-    07)
-      # sync and download
-      CLEAR
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -os ${GOGOS}
-      ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh
-      PAUSE
-      ;;
-    *)
-      exit 1
-      ;;
-    esac
-  done
-
+  \nPlease choose a task" ${MG} 10 \
+  "01" "Exit to previous menu" \
+  "02" "Configure my GOG credentials" \
+  "03" "Change my GOG Operating System setting" \
+  "04" "Synchronise my games list" \
+  "05" "Download/update a single game" \
+  "06" "Download/update all games currently in my games list" \
+  "07" "FULL - Synchronise games list & download/update all games" \
+  2> ${TDIR}/rn_gog_chooser
 }
 
 rn_gog_setos() {
-  source $_CONFIG
-
-  local MENU_ARRAY=(
-    01 "Exit to previous menu"
-    02 "Windows only"
-    03 "Mac only"
-    04 "Linux only"
-    05 "Windows & Mac"
-    06 "Windows & Linux"
-    07 "Mac & Linux"
-    08 "ALL Operating Systems"
-  )
-
-  local MENU_BLURB="Current Operating Systems: ${OLDGOGOS} \
+source /opt/retronas/dialog/retronas.cfg
+dialog \
+  --backtitle "RetroNAS" \
+  --title "gogrepo configure OS" \
+  --clear \
+  --menu "Current Operating Systems: ${OLDGOGOS} \
   \n
-  \nPlease set the Operating System(s) you would like to download GOG games for."
-
-  DLG_MENU "gogrepo configure OS" $MENU_ARRAY 10 "${MENU_BLURB}"
-
-  case ${CHOICE} in
-    02)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "windows"' >>"${ANCFG}"
-      ;;
-    03)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "mac"' >>"${ANCFG}"
-      ;;
-    04)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "linux"' >>"${ANCFG}"
-      ;;
-    05)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "windows mac"' >>"${ANCFG}"
-      ;;
-    06)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "windows linux"' >>"${ANCFG}"
-      ;;
-    07)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "mac linux"' >>"${ANCFG}"
-      ;;
-    08)
-      sed -i '/retronas_gog_os:/d' "${ANCFG}"
-      echo 'retronas_gog_os: "windows mac linux"' >>"${ANCFG}"
-      ;;
-    *)
-      exit 0
-      ;;
-  esac
+  \nPlease set the Operating System(s) you would like to download GOG games for." ${MG} 10 \
+  "01" "Exit to previous menu" \
+  "02" "Windows only" \
+  "03" "Mac only" \
+  "04" "Linux only" \
+  "05" "Windows & Mac" \
+  "06" "Windows & Linux" \
+  "07" "Mac & Linux" \
+  "08" "ALL Operating Systems" \
+  2> ${TDIR}/rn_gog_setos
+NEWGOGOS=$( cat ${TDIR}/rn_gog_setos )
+case ${NEWGOGOS} in
+  02)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "windows"' >>"${ANCFG}"
+    ;;
+  03)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "mac"' >>"${ANCFG}"
+    ;;
+  04)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "linux"' >>"${ANCFG}"
+    ;;
+  05)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "windows mac"' >>"${ANCFG}"
+    ;;
+  06)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "windows linux"' >>"${ANCFG}"
+    ;;
+  07)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "mac linux"' >>"${ANCFG}"
+    ;;
+  08)
+    sed -i '/retronas_gog_os:/d' "${ANCFG}"
+    echo 'retronas_gog_os: "windows mac linux"' >>"${ANCFG}"
+    ;;
+  *)
+    exit 0
+    ;;
+esac
 }
 
 rn_gog_gameslist() {
-  rm ${TDIR}/rn_gog_gameslist 2>/dev/null
-  RNUDIR=$( getent passwd ${OLDRNUSER} | awk -F ':' '{print $6}' )
-  grep \'title\'\: ${RNUDIR}/.gogrepo/gog-manifest.dat | awk -F \' '{print $4}' | sort | while read RN_GOG_ID
-  do
-    echo -en "${RN_GOG_ID} . " >>${TDIR}/rn_gog_gameslist
-  done
+rm ${TDIR}/rn_gog_gameslist 2>/dev/null
+RNUDIR=$( getent passwd | grep ^${OLDRNUSER}\: | awk -F ':' '{print $6}' )
+grep \'title\'\: ${RNUDIR}/.gogrepo/gog-manifest.dat | awk -F \' '{print $4}' | sort | while read RN_GOG_ID
+do
+  echo -en "${RN_GOG_ID} . " >>${TDIR}/rn_gog_gameslist
+done
 }
 
 rn_gog_game() {
-
-  local MENU_ARRAY=(
-    $(cat ${TDIR}/rn_gog_gameslist)
-  )
-
-  local MENU_BLURB="Please choose a game \
+dialog \
+  --backtitle "RetroNAS" \
+  --title "gogrepo game chooser" \
+  --clear \
+  --menu "Please choose a game \
   \n
-  \nIf your game has not appeared, please synchronise your games list"
-
-  DLG_MENU "gogrepo game chooser" $MENU_ARRAY 10 "${MENU_BLURB}"
-
+  \nIf your game has not appeared, please synchronise your games list" ${MG} 10 \
+  $(cat ${TDIR}/rn_gog_gameslist) 2> ${TDIR}/rn_gog_game
 }
 
-
-CLEAR
-rn_gog_chooser
+while true
+do
+  source /opt/retronas/dialog/retronas.cfg
+  rn_gog_chooser
+  CHOICE=$( cat ${TDIR}/rn_gog_chooser )
+  PAUSEMSG='Press [Enter] to continue...'
+  case ${CHOICE} in
+  02)
+    # login
+    clear
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_login.sh
+    echo "${PAUSEMSG}"
+    read -s
+    ;;
+  03)
+    # OS
+    rn_gog_setos
+    ;;
+  04)
+    # sync games list
+    clear
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -skipknown -os ${OLDGOGOS}
+    echo "${PAUSEMSG}"
+    read -s
+    ;;
+  05)
+    # download 1 game
+    rn_gog_gameslist
+    rn_gog_game
+    GOGGAME=$( cat ${TDIR}/rn_gog_game )
+    clear
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -os ${OLDGOGOS} -id ${GOGGAME}
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh -id ${GOGGAME}
+    echo "${PAUSEMSG}"
+    read -s
+    ;;
+  06)
+    # download all games
+    clear
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh
+    echo "${PAUSEMSG}"
+    read -s
+    ;;
+  07)
+    # sync and download
+    clear
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_update.sh -os ${GOGOS}
+    ${SUCOMMAND} ${RNDIR}/scripts/gogrepo_download.sh
+    echo "${PAUSEMSG}"
+    read -s
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
--- dialog/retronas_fixperms.sh
+++ dialog/retronas_fixperms.sh
@@ -1,53 +1,47 @@
 #!/bin/bash
 
-set -u
+clear
 
-_CONFIG=/opt/retronas/dialog/retronas.cfg
-source $_CONFIG
-source ${DIDIR}/common.sh
+source /opt/retronas/dialog/retronas.cfg
 cd ${DIDIR}
 
 rn_get_dirs() {
-  COUNT=2
-  cd "${OLDRNPATH}"
-  find . -maxdepth 1 -type d | sed 's#^\./##g' | grep -v ^\.$ | sort | while read PATHITEM
-  do
-    echo "${PATHITEM}" "${PATHITEM}"
-    COUNT=$((${COUNT}+1))
-  done
+cd "${OLDRNPATH}"
+find . -maxdepth 1 -type d | sed 's#^\./##g' | grep -v ^\.$ | sort | while read PATHITEM
+do
+  echo "${PATHITEM}" "${PATHITEM}" >> ${TDIR}/rn_get_dirs
+  COUNT=$((${COUNT}+1))
+done
+
+cat ${TDIR}/rn_get_dirs
 }
 
 rn_fix_perms() {
-  cd ${DIDIR}
-
-  local MENU_ARRAY=(
-    1 "Exit to main menu"
-    $(rn_get_dirs)
-  )
-
-  # Append directory items
-  #DIRS=$(rn_get_dirs)
-  #COUNT=01
-  #for DIR in ${DIRS[@]}
-  #do
-  #  MENU_ARRAY+=($((++COUNT)) "$DIR")
-  #done
-
-  local MENU_BLURB="\nPlease choose a directory below ${OLDRNPATH} to fix. \
-  \n\nThis will reset all the file ownership to the user \"${OLDRNUSER}\" \
-  \n\nPlease be careful, as this is irreversible.  If unsure, exit now."
-
-  DLG_MENU "Fix Permissions" $MENU_ARRAY 5 "${MENU_BLURB}" 
+cd ${DIDIR}
+dialog \
+  --backtitle "RetroNAS" \
+  --title "RetroNAS Fix Permissions" \
+  --clear \
+  --menu "Please choose a directory below ${OLDRNPATH} to fix. \
+  \n
+  \nThis will reset all the file ownership to the user \"${OLDRNUSER}\" \
+  \n
+  \nPlease be careful, as this is irreversible.  If unsure, exit now." \
+  ${MG} 5 \
+  "1" "Exit to main menu" \
+  $( cat  ${TDIR}/rn_get_dirs ) 2>${TDIR}/rn_fix_perms
 }
 
-CLEAR
+rn_get_dirs
+
 rn_fix_perms
+CHOICE=$( cat ${TDIR}/rn_fix_perms )
 case ${CHOICE} in
   1)
     exit 1
     ;;
   *)
-    CLEAR
+    clear
     chown -Rc ${OLDRNUSER}:${OLDRNUSER} "${OLDRNPATH}/${CHOICE}"
     chmod -Rc a-st,u+rwX,g+rwX,o+rX "${OLDRNPATH}/${CHOICE}"
     ;;
