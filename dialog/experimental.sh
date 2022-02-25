73d34cb409209790ebeeffcd64a3fbd437228b19
sairuk <sairuk@gmail.com>
2022-02-26 00:49:19 +1100

    add cockpit as a dependancy of the xboxmanager install

--- dialog/experimental.sh
+++ dialog/experimental.sh
@@ -37,6 +37,9 @@ do
     CLEAR
     RN_INSTALL_DEPS
     RN_INSTALL_EXECUTE install_filesystems.yml
+    RN_INSTALL_EXECUTE install_cockpit.yml
+    RN_INSTALL_EXECUTE install_cockpit-packages.yml
+    RN_INSTALL_EXECUTE install_filesystems.yml
     RN_INSTALL_EXECUTE install_extract-xiso.yml
     RN_INSTALL_EXECUTE install_xboxmanager.yml
     PAUSE
