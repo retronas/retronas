fced5490b850073cb4047c65eea51b5f85f05114
sairuk <sairuk@gmail.com>
2022-02-27 12:12:39 +1100

    update preseed installer

--- dist/install_preseed.sh
+++ dist/install_preseed.sh
@@ -5,8 +5,10 @@
 # as part of d-i preseed/late_command
 #
 
-
+curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
+chmod a+x /tmp/install_retronas.sh
 /tmp/install_retronas.sh
+
 cd /opt/retronas/ansible
 ansible-playbook -vv retronas_dependencies.yml
 ansible-playbook -vv install_filesystems.yml
