20c6f2c0dd2604b88a418adc3fa7e1ae39ed1b9f
sairuk <sairuk@gmail.com>
2022-02-27 19:09:36 +1100

    still playing with preseeding

--- dist/install_preseed.sh
+++ dist/install_preseed.sh
@@ -5,16 +5,21 @@
 # as part of d-i preseed/late_command
 #
 
-/usr/bin/curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
-/usr/bin/chmod a+x /tmp/install_retronas.sh
-/tmp/install_retronas.sh
+if [ -f /opt/retronas/retronas_deployed ]
+then
+    /usr/bin/curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
+    /usr/bin/chmod a+x /tmp/install_retronas.sh
+    /tmp/install_retronas.sh
 
-cd /opt/retronas/ansible
+    cd /opt/retronas/ansible
 
-cp retronas_vars.yml.default retronas_vars.yml
+    cp retronas_vars.yml.default retronas_vars.yml
 
-/usr/bin/ansible-playbook -vv retronas_dependencies.yml
-/usr/bin/ansible-playbook -vv install_filesystems.yml
-/usr/bin/ansible-playbook -vv install_cockpit.yml
-/usr/bin/ansible-playbook -vv install_cockpit-packages.yml
-#/usr/bin/ansible-playbook -vv install_cockpit-retronas.yml
+    /usr/bin/ansible-playbook -vv retronas_dependencies.yml
+    /usr/bin/ansible-playbook -vv install_filesystems.yml
+    /usr/bin/ansible-playbook -vv install_cockpit.yml
+    /usr/bin/ansible-playbook -vv install_cockpit-packages.yml
+    #/usr/bin/ansible-playbook -vv install_cockpit-retronas.yml
+
+    touch /opt/retronas/retronas_deployed
+fi
