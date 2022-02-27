3009f232dacb5416b0c03c2268c3fc28397b64d7
sairuk <sairuk@gmail.com>
2022-02-27 19:28:13 +1100

    negate the check so it runs at boot

--- dist/install_preseed.sh
+++ dist/install_preseed.sh
@@ -5,7 +5,7 @@
 # as part of d-i preseed/late_command
 #
 
-if [ -f /opt/retronas/retronas_deployed ]
+if [ ! -f /opt/retronas/retronas_deployed ]
 then
     /usr/bin/curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
     /usr/bin/chmod a+x /tmp/install_retronas.sh
