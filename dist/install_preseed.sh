4e83199b422666432ccfd056615476ae969ccaf1
sairuk <sairuk@gmail.com>
2022-02-27 12:25:21 +1100

    still playing with preseed

--- dist/install_preseed.sh
+++ dist/install_preseed.sh
@@ -5,13 +5,13 @@
 # as part of d-i preseed/late_command
 #
 
-curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
-chmod a+x /tmp/install_retronas.sh
+/usr/bin/curl -so /tmp/install_retronas.sh https://raw.githubusercontent.com/danmons/retronas/main/install_retronas.sh
+/usr/bin/chmod a+x /tmp/install_retronas.sh
 /tmp/install_retronas.sh
 
 cd /opt/retronas/ansible
-ansible-playbook -vv retronas_dependencies.yml
-ansible-playbook -vv install_filesystems.yml
-ansible-playbook -vv install_cockpit.yml
-ansible-playbook -vv install_cockpit-packages.yml
-ansible-playbook -vv install_cockpit-retronas.yml
+/usr/bin/ansible-playbook -vv retronas_dependencies.yml
+/usr/bin/ansible-playbook -vv install_filesystems.yml
+/usr/bin/ansible-playbook -vv install_cockpit.yml
+/usr/bin/ansible-playbook -vv install_cockpit-packages.yml
+/usr/bin/ansible-playbook -vv install_cockpit-retronas.yml
