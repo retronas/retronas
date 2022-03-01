5cb5cff108788ac8908e82b311c2a4ee5aed9242
sairuk <sairuk@gmail.com>
2022-03-01 21:40:27 +1100

    Change launch structure for webui

--- lib/ansible_runner.sh
+++ lib/ansible_runner.sh
@@ -10,27 +10,24 @@ source $_CONFIG
 
 PREFIX=install_
 SUFFIX=.yml
-
 ANSCMD="/usr/bin/ansible-playbook"
 
 cd "${ANDIR}"
 
-[ -z "${1}" ] && echo "No options passed" && exit 1
-[ ! -f ${ANSCMD} ] && echo "Ansible is not installed, install it first" && exit 1
-
-IFS=';' read -r -a PLAYBOOKS <<< "${1}"
-
-[ ${PLAYBOOKS[#]} -eq 0 ] && echo "Failed to read args, exiting"  && exit 1
-
 function run_ansible {
-    local PLAYBOOK="${ANDIR}/${PREFIX}${1}${SUFFIX}"
-
-    [ ! -f ${PLAYBOOK} ] && echo "Failed to find playbook for $1" && exit 1
+    local PLAYBOOK="${1}"
+    [ ! -f ${PLAYBOOK} ] && echo "Failed to find playbook: ${PLAYBOOK}" && exit 1
     ${ANSCMD} -vv "${PLAYBOOK}"
 }
 
-run_ansible retronas_dependancies
+[ -z "${1}" ] && echo "No options passed" && exit 1
+[ ! -f ${ANSCMD} ] && echo "Ansible is not installed, install it first" && exit 1
+
+IFS=':' read -r -a PLAYBOOKS <<< "${1}"
+[ ${#PLAYBOOKS[@]} -eq 0 ] && echo "Failed to read args, exiting"  && exit 1
+
+run_ansible ${ANDIR}/retronas_dependencies.yml
 for PLAYBOOK in ${PLAYBOOKS[@]}
 do
-    run_ansible $PLAYBOOK
+    run_ansible "${ANDIR}/${PREFIX}${PLAYBOOK}${SUFFIX}"
 done
