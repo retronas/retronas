8459a0d926f4169f9c12dca21246a9c7435a22f3
sairuk <sairuk@gmail.com>
2022-12-11 20:02:58 +1100

    tweak some dex stuff

--- dialog/dexdrive_serial_edit.sh
+++ dialog/dexdrive_serial_edit.sh
@@ -9,13 +9,16 @@ cd ${DIDIR}
 [ ! -f ${SERVICE_CONFIG} ] && echo "Install adtpro first" && PAUSE
 
 # DEFAULTS
-DEVICE="/dev/ttyS0"
+DEVICE="/dev/ttyUSB0"
 
 source $_CONFIG
 cd ${DIDIR}
 
 rn_dexdrive_serial_edit() {
+
   READ_MENU_TDESC "${MENU_NAME}"
+
+  DEXDRIVE=$(echo $DEVICE | sed 's#^/dev/##')
 
   MENU_ARRAY=(
     "Serial Device:"       1 5 "$DEVICE"     1 27 20 20
@@ -23,7 +26,11 @@ rn_dexdrive_serial_edit() {
 
   DLG_FORM "${MENU_TNAME}" "${MENU_ARRAY}" 8 "${MENU_BLURB}"
 
-  [ ${#CHOICE[@]}  -gt 0 ] && export DEXDRIVE=$(echo $DEVICE | sed 's#^/dev/##')
+  if [ ${#CHOICE[@]} -gt 0 ] 
+  then 
+      sed -r "s#^(.*)\s(.*)\s(.*)#\1 \2 ${CHOICE[0]}#" -i /etc/systemd/system/linux-dexdrive.service
+      systemctl daemon-reload
+  fi
   EXIT_OK
 
 }
