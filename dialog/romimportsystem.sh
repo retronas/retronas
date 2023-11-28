e259b2ecc21b2bd4161b1e4a2bf82b1ec7950c99
David Calawa <blathersbugs@gmail.com>
2023-11-28 14:12:56 -0800

    debug

--- dialog/romimportsystem.sh
+++ dialog/romimportsystem.sh
@@ -19,13 +19,14 @@ rn_import_system() {
   
 
     while read -r line; do
-        echo $line
+        # echo $line
         SYSTEMS+=${line[1]}
     done < <(/opt/retronas/scripts/romimport.sh -l)
 
     i=2
     for s in $SYSTEMS:
     do
+        echo "system"
         echo $s
         SYSTEM_ARR[i]=$s
         ((++i))
