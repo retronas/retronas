def358a42325b9096ddf9ba87d272b8d1f8b185f
David Calawa <blathersbugs@gmail.com>
2023-11-29 10:26:18 -0800

    new menu system; reduce duplication

--- dialog/romimportsystem.sh
+++ dialog/romimportsystem.sh
@@ -17,20 +17,20 @@ rn_import_system() {
   DLG_MENUJ "${MENU_TNAME}" 10 "${MENU_BLURB}"
   
 
-    while read -r line; do
-        lineArr=($line)
-        STR=${lineArr[0]}
+    while read -r LINE; do
+        LINE_ARR=($LINE)
+        STR=${LINE_ARR[0]}
         SUB="id"
         if [[ "$STR" == *"$SUB"* ]]; then
-            SYSTEMS+=${lineArr[1]}' '
+            SYSTEMS+=${LINE_ARR[1]}' '
         fi
     done < <(/opt/retronas/scripts/romimport.sh -l)
 
-    i=2
+    I=2
     for s in $SYSTEMS:
     do
-        SYSTEM_ARR[$i]=$s
-        ((++i))
+        SYSTEM_ARR[$I]=$s
+        ((++I))
     done
 
   while true
@@ -41,151 +41,151 @@ rn_import_system() {
       01)
         EXIT_OK
       ;;
-      02)
+      [02-30])
         CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[2]}
+        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[$CHOICE]}
         PAUSE
-      ;;
-      03)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[3]}
-        PAUSE
-      ;;
-      04)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[4]}
-        PAUSE
-      ;;
-      05)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[5]}
-        PAUSE
-      ;;
-      06)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[6]}
-        PAUSE
-      ;;
-      07)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[7]}
-        PAUSE
-      ;;
-      08)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[8]}
-        PAUSE
-      ;;
-      09)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[9]}
-        PAUSE
-      ;;
-      10)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[10]}
-        PAUSE
-      ;;
-      11)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[11]}
-        PAUSE
-      ;;
-      12)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[12]}
-        PAUSE
-      ;;
-      13)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[13]}
-        PAUSE
-      ;;
-      14)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[14]}
-        PAUSE
-      ;;
-      15)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[15]}
-        PAUSE
-      ;;
-      16)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[16]}
-        PAUSE
-      ;;
-      17)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[17]}
-        PAUSE
-      ;;
-      18)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[18]}
-        PAUSE
-      ;;
-      19)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[19]}
-        PAUSE
-      ;;
-      20)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[20]}
-        PAUSE
-      ;;
-      21)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[21]]}
-        PAUSE
-      ;;
-      22)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[22]}
-        PAUSE
-      ;;
-      23)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[23]}
-        PAUSE
-      ;;
-      24)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[24]}
-        PAUSE
-      ;;
-      25)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[25]}
-        PAUSE
-      ;;
-      26)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[26]}
-        PAUSE
-      ;;
-      27)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[27]}
-        PAUSE
-      ;;
-      28)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[28]}
-        PAUSE
-      ;;
-      29)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[29]}
-        PAUSE
-      ;;
-      30)
-        CLEAR
-        /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[30]}
-        PAUSE
-      ;;
+      # ;;
+      # 03)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[3]}
+      #   PAUSE
+      # ;;
+      # 04)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[4]}
+      #   PAUSE
+      # ;;
+      # 05)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[5]}
+      #   PAUSE
+      # ;;
+      # 06)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[6]}
+      #   PAUSE
+      # ;;
+      # 07)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[7]}
+      #   PAUSE
+      # ;;
+      # 08)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[8]}
+      #   PAUSE
+      # ;;
+      # 09)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[9]}
+      #   PAUSE
+      # ;;
+      # 10)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[10]}
+      #   PAUSE
+      # ;;
+      # 11)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[11]}
+      #   PAUSE
+      # ;;
+      # 12)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[12]}
+      #   PAUSE
+      # ;;
+      # 13)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[13]}
+      #   PAUSE
+      # ;;
+      # 14)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[14]}
+      #   PAUSE
+      # ;;
+      # 15)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[15]}
+      #   PAUSE
+      # ;;
+      # 16)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[16]}
+      #   PAUSE
+      # ;;
+      # 17)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[17]}
+      #   PAUSE
+      # ;;
+      # 18)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[18]}
+      #   PAUSE
+      # ;;
+      # 19)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[19]}
+      #   PAUSE
+      # ;;
+      # 20)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[20]}
+      #   PAUSE
+      # ;;
+      # 21)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[21]]}
+      #   PAUSE
+      # ;;
+      # 22)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[22]}
+      #   PAUSE
+      # ;;
+      # 23)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[23]}
+      #   PAUSE
+      # ;;
+      # 24)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[24]}
+      #   PAUSE
+      # ;;
+      # 25)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[25]}
+      #   PAUSE
+      # ;;
+      # 26)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[26]}
+      #   PAUSE
+      # ;;
+      # 27)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[27]}
+      #   PAUSE
+      # ;;
+      # 28)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[28]}
+      #   PAUSE
+      # ;;
+      # 29)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[29]}
+      #   PAUSE
+      # ;;
+      # 30)
+      #   CLEAR
+      #   /opt/retronas/scripts/romimport.sh -t ${SYSTEM_ARR[30]}
+      #   PAUSE
+      # ;;
       *)
       EXIT_CANCEL
       ;;
